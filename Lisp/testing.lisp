ce9e19604c1c69d7906142f2a3a898944b8d441f
Timothy Wilson <blaareproductions@gmail.com>
2018-09-12 23:43:31 -0400

    removed unnecessary files

--- Lisp/testing.lisp
+++
@@ -1,5 +0,0 @@
-(defun deep-reverse(L)
-  "This function performs a deep reverse, recursively"
-  (cond(( endp L)    nil)
-       (( listp L) ( append( deep-reverse( rest L)) ( list ( first L)))))
-       (t          ( append( deep-reverse( rest L)) ( list ( first L))))))
